102402574d7025411773e7d9c80fd681d9b6e503
hrishi585 <hakathikar@gmail.com>
2021-04-16 09:26:26 +0530

    first commit

---
+++ src/pages/landing-page/landing-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../nav-bar/nav-bar";
+
+import "./landing-page.css"
+
+function LandingPage(){
+    return(
+    <div className="nav">
+        <Navbar/> 
+    </div>
+    )
+}
+
+export default LandingPage; 
